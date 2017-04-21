308c50fd4c93c11dc918b3ab2ad2edf1cf6f1393
Alex Perez <alexperez.dev@gmail.com>
2017-04-21 15:40:28 +1200

    New views, changes in styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AboutComponent } from './about/about.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
 const routes: Routes = [
   {
     path: '',
-    children: []
+    redirectTo: '/about',
+    pathMatch: 'full'
+  }, {
+  	path: 'about',
+  	component: AboutComponent
+  }, {
+  	path: 'technologies',
+  	component: TechnologiesComponent
+  }, {
+  	path: 'projects',
+  	component: ProjectsComponent
+  }, {
+  	path: 'contact',
+  	component: ContactComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PhoneMenuComponent } from './phone-menu/phone-menu.component';
+import { AboutComponent } from './about/about.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PhoneMenuComponent
+    PhoneMenuComponent,
+    AboutComponent,
+    TechnologiesComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
